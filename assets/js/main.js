eed7b6a8695930a6a56e9f66a2d87277e5b28acd
kumelkalen <free.your.mind.fc@gmail.com>
2017-05-07 22:38:44 -0300

    Después de nuestras propiedades creamos los métodos comenzando por el de presentación de pokémon donde haremos una breve descripción del pokémon.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-main.js
+function Pokemon(nombre, color, ataque, vida) {
+    this.nombre = nombre;
+    this.color = color;
+    this.amistad = 0;
+    this.vida = vida;
+    this.ataque = ataque;
+
+}
